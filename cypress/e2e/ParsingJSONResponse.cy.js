26f18cee9411ac59eae17255651cdd711a5e4501
Dar1aH <daxundaintravel@gmail.com>
2023-11-25 19:07:08 +0000

    update

---
+++ cypress/e2e/ParsingJSONResponse.cy.js
@@ -0,0 +1,48 @@
+describe("Parsing JSON Response", ()=>{
+
+    it("Parsing simple JSON response", ()=>{
+
+        cy.request(
+            {
+                method: 'GET',
+                url: "http://fakestoreapi.com/products",
+            }
+        )
+        .then((response)=>{
+            expect(response.status).to.equal(200);
+            expect(response.body[0].id).to.equal(1);
+            expect(response.body[0].title).to.equal("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops");
+            expect(response.body[0].price).to.equal(109.95);
+            expect(response.body[0].rating.rate).to.equal(3.9);
+
+            expect(response.body[19].id).to.equal(20);
+            expect(response.body[19].title).to.equal("DANVOUY Womens T Shirt Casual Cotton Short");
+            expect(response.body[19].price).to.equal(12.99);
+            expect(response.body[19].rating.rate).to.equal(3.6);
+
+
+        })
+    })
+    // I want to retrieve data on every price in 20 objects and get the total price of all objects
+    // qs - query parameter 
+    it.only("Parsing complex JSON response", ()=>{
+
+        let totalprice=0;
+        cy.request(
+            {
+                method: 'GET',
+                url: "http://fakestoreapi.com/products",
+                qs:{limit:3}
+            }
+        )
+        .then((response)=>{
+            expect(response.status).to.equal(200)
+
+            response.body.forEach(element => {
+                totalprice=totalprice+element.price;
+            });
+            //expect(totalprice).to.equal(899.23); //limit 5 
+            expect(totalprice).to.equal(188.24); //limit 3 
+        })
+    })
+})
